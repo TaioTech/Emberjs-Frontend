7124fcfa52859eafd9f69ff9cbe983a448d04035
Admin <tao@admins-mbp.attlocal.net>
2018-06-03 08:22:59 -0500

    Google drive APIs functionality working along with authentication

--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,21 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    'ember-simple-auth': {
+      authenticationRoute: 'index'
+    },
+
+    torii: {
+      sessionServiceName: 'session',
+      providers: {
+        'google-oauth2': {
+          apiKey: '528666433412-869fq0jd6k485qnjkalfsmai7urlgetc.apps.googleusercontent.com',
+          redirectUri: 'http://localhost:4200/torii/redirect.html',
+          scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/drive'
+        }
+      }
     }
   };
 
